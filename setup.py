beefddc18e5633ac624031e0b8f77593f3643975
Peter Divos <pdivos@gmail.com>
2022-06-08 23:13:49 +0100

    adding stuff

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='domsync',
     version='0.1',
-    description='Python server-side DOM synchronised to Browser',
+    description='Python server-side DOM synchronised with Browser',
     author='pdivos',
     author_email='pdivos@gmail.com',
     license='MIT',
